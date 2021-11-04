9deafd56bbb0c4dff6c8f98e249888b18fbdcda3
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-11-04 11:44:40 -0400

    Update index.js

--- 11-3-21/index.js
+++ 11-3-21/index.js
@@ -1,16 +1,20 @@
 function getCount(str) {
-    var vowelsCount = 0;
-    
-    for(const char of str){
-        if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char =='u'){
-            vowelsCount += 1
-        }
+  var vowelsCount = 0;
+
+  for (const char of str) {
+    if (
+      char == "a" ||
+      char == "e" ||
+      char == "i" ||
+      char == "o" ||
+      char == "u"
+    ) {
+      vowelsCount += 1;
     }
-    
-    return vowelsCount;
   }
 
-
+  return vowelsCount;
+}
 
 //console.log(getCount('acceiggrtaiuo')); // 7 vowels
 
@@ -24,18 +28,17 @@ function getCount(str) {
 //  4 |  500
 //  5 |  750
 //  6 | 1050
-// Can you find a pattern in it? If so, then write a 
+// Can you find a pattern in it? If so, then write a
 // function getScore(n)/get_score(n)/GetScore(n) which returns the score for any positive number n:
 
 function getScore(n) {
-    let adder = 50
-    let score = 0;
-    for(let i=1; i <= n; i++){    
-        score += adder
-        adder += 50
-    }
-    return score
+  let adder = 50;
+  let score = 0;
+  for (let i = 1; i <= n; i++) {
+    score += adder;
+    adder += 50;
   }
+  return score;
+}
 
-
-  console.log(getScore(6));
+console.log(getScore(6));
