555d494c8985a7f7254cb8e7caa875af004d3e44
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-11-04 11:41:23 -0400

    Update index.js

--- 11-3-21/index.js
+++ 11-3-21/index.js
@@ -12,4 +12,30 @@ function getCount(str) {
 
 
 
-console.log(getCount('acceiggrtaiuo')); // 7 vowels
+//console.log(getCount('acceiggrtaiuo')); // 7 vowels
+
+// Have a look at the following numbers.
+
+//  n | score
+// ---+-------
+//  1 |  50
+//  2 |  150
+//  3 |  300
+//  4 |  500
+//  5 |  750
+//  6 | 1050
+// Can you find a pattern in it? If so, then write a 
+// function getScore(n)/get_score(n)/GetScore(n) which returns the score for any positive number n:
+
+function getScore(n) {
+    let adder = 50
+    let score = 0;
+    for(let i=1; i <= n; i++){    
+        score += adder
+        adder += 50
+    }
+    return score
+  }
+
+
+  console.log(getScore(6));
