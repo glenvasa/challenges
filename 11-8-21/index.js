1b6e55b8e1d962dee38f165e5569094c864e9a4a
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-11-08 10:47:24 -0500

    Create index.js

---
+++ 11-8-21/index.js
@@ -0,0 +1,22 @@
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
+
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
+
+
+
+// Test.assertEquals(seven(times(five())), 35);
+// Test.assertEquals(four(plus(nine())), 13);
+// Test.assertEquals(eight(minus(three())), 5);
+// Test.assertEquals(six(dividedBy(two())), 3);
