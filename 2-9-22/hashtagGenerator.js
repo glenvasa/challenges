13503aa70b3ef7ea1409b8a8c753e633bd231a84
glenvasa <70590043+glenvasa@users.noreply.github.com>
2022-02-11 09:53:32 -0500

    solution
    5kyu CodeWars Kata

--- 2-9-22/hashtagGenerator.js
+++ 2-9-22/hashtagGenerator.js
@@ -3,21 +3,34 @@
 // If the final result is longer than 140 chars it must return false.
 // If the input or the result is an empty string it must return false.
 
-function generateHashtag (str) {
+function generateHashtag(str) {
+  let result;
+  if (str === " ") {
+    return false;
+  }
 
+  let arr = str.trim().split(" ");
 
-    
-    
-    if(result.length > 140 || str === '' || result === ''){
-        return false
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "") {
+      arr.splice(i, 1);
+      i--;
     }
-    return result
+  }
+
+  let capArray = arr.map(
+    (word) => word[0].toUpperCase() + word.slice(1, word.length)
+  );
+
+  result = capArray.join("");
+
+  if (result.length > 139 || result === "") {
+    return false;
+  }
+
+  return `#${result}`;
 }
 
-console.log(generateHashtag(" Hello there thanks for trying my Kata"));// =>  "#HelloThereThanksForTryingMyKata"
-console.log(generateHashtag("    Hello     World   "));// =>  "#HelloWorld"
-console.log(generateHashtag(""));// =>  false
-
- 
-                 
-                                       
+console.log(generateHashtag(" Hello there thanks for trying my Kata")); // =>  "#HelloThereThanksForTryingMyKata"
+console.log(generateHashtag("    Hello     World   ")); // =>  "#HelloWorld"
+console.log(generateHashtag("")); // =>  false
